43cf91205c512675020c0eeb20d00b7a71854f8c
ricardoairesm <ricardoaamb@usp.br>
2023-01-09 11:46:57 -0300

    tweets do usuario

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 
 //criando app
 const app = express();
+const PORT = 5000;
 app.use(express.json());
 app.use(cors());
 
@@ -12,18 +13,27 @@ const usuarios = [];
 const tweets = [];
 const fullBody = [];
 
+// dividindo os tweets para a paginação
+function displayPagina(arr,pagina){
+const lastTweets = [...arr].reverse();
+lastTweets.slice((pagina-1)*10 , pagina*10);
+return lastTweets;
+}
+
 // recebendo os dados do post pela rota /sign-up
 app.post('/sign-up', (req, res) => {
+  //checando tipagem e se os campos estão vazios
   if (!username || typeof username !== 'string' || !avatar || typeof avatar !== 'string') {
     return res.status(400).send('Todos os campos são obrigatórios!');
   }
 	const usuario = req.body;
   usuarios.push(usuario);
-  res.send("OK");
+  res.status(201).send("OK");
 });
 
 //recebendo os dados do tweet enviado pela rota /tweets
 app.post('/tweets', (req, res) => {
+  //checando tipagem e se os campos estão vazios
   if (!username || typeof username !== 'string' || !tweet || typeof tweet !== 'string') {
     return res.status(400).send('Todos os campos são obrigatórios!');
   }
@@ -33,7 +43,7 @@ app.post('/tweets', (req, res) => {
   for(let i = 0;i<usuarios.length;i++){
     if(tweet.username === usuarios[i].username){
       tweets.push(tweet);
-      res.send("OK");
+      res.status(201).send("OK");
       fullBody.push({
         username: `${tweet.username}`,
         avatar: `${usuarios[i].avatar}`,
@@ -43,17 +53,28 @@ app.post('/tweets', (req, res) => {
       return;
     }   
   }
-  res.send("UNAUTHORIZED");
+  res.status(401).send("UNAUTHORIZED");
 });
 
 app.get('/tweets', (req, res) => {
-  const lastTweets = fullBody.reverse();
-  lastTweets.slice(0,10);
-  res.send(lastTweets);
+  const tweetListDisplay = [];
+  //pegando a pagina que nos encontramos
+  const page = req.query.page;
+  if(page<1){
+    res.status(400).send('Informe uma página válida!');
+    tweetListDisplay = displayPagina(fullBody,1);
+  }
+  else{
+    tweetListDisplay = displayPagina(fullBody,page);
+  }
+  res.send(tweetListDisplay);
 });
 
+app.get('/tweets/:username', (req, res)=>{
+  const userTweets = fullBody.filter(tweet => tweet.username === req.params.username);
+  res.send(userTweets);
+})
 
-
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log('Rodando Servidor!')
 })
