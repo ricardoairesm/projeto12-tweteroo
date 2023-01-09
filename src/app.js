474fd326c70ba953f293b5997664525cc1b0462c
ricardoairesm <ricardoaamb@usp.br>
2023-01-09 13:33:03 -0300

    desistindo

--- src/app.js
+++ src/app.js
@@ -59,7 +59,6 @@ app.post('/tweets', (req, res) => {
 
 app.get('/tweets', (req, res) => {
   const tweetListDisplay = fullBody.reverse();
-  tweetListDisplay = tweetListDisplay.slice(0,10);
   //pegando a pagina que nos encontramos
   const page = Number(req.query.page);
   if (page) {
